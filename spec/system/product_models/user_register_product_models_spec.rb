a0d1b78521f2ef0e6f4c984666bb526b900354ab
Lucas César <lucascesar_35@hotmail.com>
2022-05-16 02:35:02 -0300

    Bloqueando visitantes em rotas

--- spec/system/product_models/user_register_product_models_spec.rb
+++ spec/system/product_models/user_register_product_models_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe 'usuário cadastra um novo modelo de produto' do
   it 'a partir do menu inicial' do
+    user = User.create!(name: 'Lucas', email: 'lucas@email.com', password: 'password')
+    login_as(user)
     visit root_path
     within 'nav' do
       click_on 'Modelos de Produtos'
@@ -17,7 +19,8 @@ describe 'usuário cadastra um novo modelo de produto' do
     other_supplier = Supplier.create!(brand_name: 'LG', corporate_name: 'LG do Brasil LTDA',
                                      registration_number: '07317108000151', full_adress: 'Av Ibirapuera, 300',
                                      city: 'São Paulo',state: 'SP', email: 'contato@lg.com.br')
-    
+    user = User.create!(name: 'Lucas', email: 'lucas@email.com', password: 'password')
+    login_as(user)
     visit root_path
     within 'nav' do
       click_on 'Modelos de Produtos'
@@ -48,6 +51,8 @@ describe 'usuário cadastra um novo modelo de produto' do
                                   registration_number: '11112222333344',full_adress: 'Av Samsung, 1000',
                                   city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
     
+    user = User.create!(name: 'Lucas', email: 'lucas@email.com', password: 'password')
+    login_as(user)
     visit root_path
     click_on 'Modelos de Produtos'
     click_on 'Cadastrar modelo de produto'
--- spec/system/product_models/user_view_product_models_spec.rb
+++ spec/system/product_models/user_view_product_models_spec.rb
@@ -1,8 +1,23 @@
 require 'rails_helper'
 
 describe 'Usuário vê modelos de produtos' do
+  it 'se estiver autenticado' do
+    visit root_path
+    within 'nav' do
+      click_on 'Modelos de Produtos'
+    end
+    expect(current_path).to eq new_user_session_path
+  end
+
   it 'a partir do menu' do
+    User.create!(name: 'Lucas', email: 'lucas@email.com', password: 'password')
     visit(root_path)
+    click_on 'Entrar'
+    fill_in 'Email', with: 'lucas@email.com'
+    fill_in 'Senha', with: 'password'
+    within('form') do
+      click_on 'Entrar'
+    end
     within('nav') do
       click_on 'Modelos de Produtos'
     end
@@ -18,7 +33,14 @@ describe 'Usuário vê modelos de produtos' do
                          sku: 'TV32-SAMS-UMG1-23456', supplier: my_supplier)
     ProductModel.create!(name: 'SoundBar 7.1 Surround', weight: 3000, width: 80, height: 15,
                          depth: 20, sku: 'SOU71-SAMSU-NOIZ7789', supplier: my_supplier)
+    User.create!(name: 'Lucas', email: 'lucas@email.com', password: 'password')
     visit(root_path)
+    click_on 'Entrar'
+    fill_in 'Email', with: 'lucas@email.com'
+    fill_in 'Senha', with: 'password'
+    within('form') do
+      click_on 'Entrar'
+    end    
     within('nav') do
       click_on 'Modelos de Produtos'
     end
@@ -32,7 +54,14 @@ describe 'Usuário vê modelos de produtos' do
   end
 
   it 'e não existem produtos cadastrados' do
-    visit root_path
+    User.create!(name: 'Lucas', email: 'lucas@email.com', password: 'password')
+    visit(root_path)
+    click_on 'Entrar'
+    fill_in 'Email', with: 'lucas@email.com'
+    fill_in 'Senha', with: 'password'
+    within('form') do
+      click_on 'Entrar'
+    end
     click_on 'Modelos de Produtos'
     expect(page).to have_content 'Nenhum modelo de produto cadastrado'
   end
